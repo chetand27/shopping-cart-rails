5c0348e438dd896dc0c47799da88acd8e7714bc4
chetan dhatrak <ketandhatrak@gmail.com>
2018-06-25 18:38:21 +0530

    Updated validation and added messages in yml file

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def sign_up
-    unless params[:user][:email].present? && params[:user][:password].present?
-      return render json: { errors: 'Please make sure, `Email` and `Password` must be present.' }
+    unless params[:user][:email].present?
+      return render json: { errors: I18n.t('errors.user.presence.email') }
+    end
+    unless params[:user][:password].present?
+      return render json: { errors: I18n.t('errors.user.presence.password') }
     end
     @user = User.new(user_params)
     return render :json => { :errors => @user.errors.full_messages } unless @user.save
