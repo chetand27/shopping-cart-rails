eb71251565e53a7b538d6608bec01c93fe94c856
chetan dhatrak <ketandhatrak@gmail.com>
2018-06-25 17:47:15 +0530

    Added User Sign Up Api.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def sign_up
-  def
+    unless params[:user][:email].present? && params[:user][:password].present?
+      return render json: { errors: 'Please make sure, `Email` and `Password` must be present.' }
+    end
+    @user = User.new(user_params)
+    return render :json => { :errors => @user.errors.full_messages } unless @user.save
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
