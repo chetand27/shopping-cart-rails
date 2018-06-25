27ae22b76a7dd47f5e6429d584a7a297367c7d71
chetan dhatrak <ketandhatrak@gmail.com>
2018-06-25 16:25:33 +0530

    Added Users Controller, Model and Route for Sign_up Method.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, constraints: {format: :json} do
+    namespace :v1 do
+      resource :users, only: [:index] do
+        collection do
+          post 'sign_up', to: 'users#sign_up'
+        end
+      end
+    end
+  end
 end
