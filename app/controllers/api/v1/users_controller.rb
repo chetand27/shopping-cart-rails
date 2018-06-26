99beab462e8a4403d3d961c36d15a37d40157283
chetan dhatrak <ketandhatrak@gmail.com>
2018-06-26 15:08:02 +0530

    Chaged scope of error msg.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def sign_up
-    unless params[:user][:email].present?
-      return render json: { errors: I18n.t('errors.user.presence.email') }
-    end
-    unless params[:user][:password].present?
-      return render json: { errors: I18n.t('errors.user.presence.password') }
+    unless params[:user][:email].present? && params[:user][:password].present?
+      return render json: { errors: [I18n.t('errors.user.presence')] }
     end
     @user = User.new(user_params)
     return render :json => { :errors => @user.errors.full_messages } unless @user.save
