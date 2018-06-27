ecfd7e76f4cd89bc1c79bfa2cfc83aebb5797b82
chetan dhatrak <ketandhatrak@gmail.com>
2018-06-27 17:33:44 +0530

    Added First Name and Last Name to User.

--- db/migrate/20180625131738_add_fields_to_users.rb
+++ db/migrate/20180625131738_add_fields_to_users.rb
@@ -2,5 +2,7 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :confirmation_at, :datetime, default: nil
     add_column :users, :otp, :integer, limt: 6
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
